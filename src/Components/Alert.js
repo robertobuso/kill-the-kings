1a3a155ff3297dcd3c45d542f59cff0629dff3b7
robertobuso <robertobuso@gmail.com>
2019-01-03 19:15:45 -0400

    working on switch function for alert portal content

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
-import { Button, Modal } from 'semantic-ui-react'
+import { Button, TransitionablePortal, Image, Segment, Header } from 'semantic-ui-react'
 
 class Alert extends Component {
 
+  alertContent = () => {
+    switch (this.props.alertKind) {
+      case 'talon':
+        return 'You must play the current card first.';
+        break;
+      
+    }
+  }
   render() {
-
     return (
-      <Modal size='mini' open={this.props.alertStatus} >
-        <Modal.Header>You must play the current card first.</Modal.Header>
-          <Modal.Content>
-            <p>Are you sure you want to delete your account</p>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button positive icon='checkmark' labelPosition='right' content='Continue'
-            onClick={this.props.clearAlert} />
-          </Modal.Actions>
-        </Modal>
+    <TransitionablePortal size='mini'       open={this.props.alertStatus}
+    transition={{ animation: 'zoom', duration: 500 }} >
+      <Segment style={{ left: '40%', position: 'fixed', top: '40%', zIndex: 1000 }}>
+        <Image wrapped size='medium' src={require('../Images/OtherImages/killthekings_header.png')} />
+        <Header>{this.alertContent()}</Header>
+        <br/>
+        <Button basic color='green' floated='right' content='Continue'
+          onClick={this.props.clearAlert} />
+      </Segment>
+    </TransitionablePortal>
     )
   }
 }
