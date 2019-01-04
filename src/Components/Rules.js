05687c9064b3f4504ec78d3315491cdbebe80cf0
robertobuso <robertobuso@gmail.com>
2019-01-04 19:48:56 -0400

    full rules open in new tab and rephrased cheat sheet

--- src/Components/Rules.js
+++ src/Components/Rules.js
@@ -11,13 +11,13 @@ class Rules extends Component {
       <Segment style={{ left: '40%', position: 'fixed', top: '40%', zIndex: 1000 }}>
 
         <Header textAlign='center'>HOW TO KILL A KING</Header>
-        <p>3 in a row of the same number</p>
-        <p>4 in a row, descending value, same suit</p>
-        <p>5 in a row, descending value, different color</p>
+        <p>Place 3 cards in a row of the same number.</p>
+        <p>Place 4 cards in a row of descending value and same suit.</p>
+        <p>Place 5 cards in a row of descending value but different color.</p>
         <p>The play's the thing wherein you'll catch the conscience of the king.</p>
         <br/>
 
-      <a href='https://docs.google.com/document/d/1QnmFhW6lRC-ZG1BkAZeG0tQIm4EjrSKSBKCJ9a5Wctk/edit'>
+      <a href='https://docs.google.com/document/d/1QnmFhW6lRC-ZG1BkAZeG0tQIm4EjrSKSBKCJ9a5Wctk/edit' target="_blank">
       <Button size='tiny' floated='left' basic color='green'>Full Rules</Button>
       </a>
       <Button size='tiny' floated='right' basic color='green' onClick={() => this.props.handleClose(false)}>Continue</Button>
