40424ed48c6f6a08949b15b7165eef83159243d2
robertobuso <robertobuso@gmail.com>
2019-01-08 19:40:17 -0500

    added a bit of css to temp home page

--- src/Routes/MainMenu.js
+++ src/Routes/MainMenu.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Button, Header, Container } from 'semantic-ui-react'
 
 class MainMenu extends Component {
 
   render() {
-    return <div>
-    WHAT THE FUCK YO
+    return <Container textAlign='center'>
     <br/><br/>
-    <NavLink to='/game'>Play Game yo</NavLink>
-    </div>
+    <Header>
+    This Will Be the Home Page
+    </Header>
+    <br/>
+    <NavLink to='/game'>Play Game</NavLink>
+    <a href='https://docs.google.com/document/d/1QnmFhW6lRC-ZG1BkAZeG0tQIm4EjrSKSBKCJ9a5Wctk/edit' target="_blank" rel="noopener noreferrer">
+    <br/><br/>
+    <Button size='tiny' basic color='green'>Full Rules</Button>
+    </a>
+    </Container>
   }
 
 }
