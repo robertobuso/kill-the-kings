0f4643e249cd6f39af9ffe94384d9a1e4b081a24
robertobuso <robertobuso@gmail.com>
2019-01-09 21:03:58 -0500

    added tutorial button to rules portal

--- src/Components/Rules.js
+++ src/Components/Rules.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { TransitionablePortal, Segment, Header, Button } from 'semantic-ui-react'
+import { TransitionablePortal, Grid, Header, Button, Segment } from 'semantic-ui-react'
+import { withRouter } from 'react-router-dom';
 
 class Rules extends Component {
+
+  handleTutorialClick = () => {
+    this.props.history.push('/tutorial')
+  }
 
   render() {
     return                            <TransitionablePortal
@@ -14,17 +19,27 @@ class Rules extends Component {
         <p>Place 3 cards in a row of the same number.</p>
         <p>Place 4 cards in a row of descending value and same suit.</p>
         <p>Place 5 cards in a row of descending value but different color.</p>
-        <p>The play's the thing wherein you'll catch the conscience of the king.</p>
         <br/>
-
+      <Grid columns={3}>
+      <Grid.Row>
+      <Grid.Column>
       <a href='https://docs.google.com/document/d/1QnmFhW6lRC-ZG1BkAZeG0tQIm4EjrSKSBKCJ9a5Wctk/edit' target="_blank" rel="noopener noreferrer">
-      <Button size='tiny' floated='left' basic color='green'>Full Rules</Button>
+      <Button size='tiny' basic color='green'>Full Rules</Button>
       </a>
-      <Button size='tiny' floated='right' basic color='green' onClick={() => this.props.handleClose(false)}>Continue</Button>
+      </Grid.Column>
+      <Grid.Column>
+      <Button size='tiny' basic color='green' onClick={this.handleTutorialClick}>Tutorial</Button>
+      </Grid.Column>
+      <Grid.Column>
+      <Button size='tiny' basic color='green' onClick={() => this.props.handleClose(false)}>Continue</Button>
+      </Grid.Column>
+      </Grid.Row>
+      </Grid>
+      <br/>
       </Segment>
     </TransitionablePortal>
   }
 
 }
 
-  export default Rules;
+  export default withRouter(Rules);
