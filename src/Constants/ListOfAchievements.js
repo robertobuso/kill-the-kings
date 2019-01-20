4d23418e6c95bd3e8d7f6e3229673a6730f409b2
robertobuso <robertobuso@gmail.com>
2019-01-20 16:20:19 -0500

    new style for achievements page done

---
+++ src/Constants/ListOfAchievements.js
@@ -0,0 +1,31 @@
+//IndexId, Menu Title, Description
+export const listOfAchievements= [
+  {id: 0, title: 'Games Won', description: ''},
+  {id: 1, title: 'Win Percentage', description: ''},
+  {id: 2, title: 'King Killed by Same Value in a Row', description: ''},
+  {id: 3, title: 'King Killed by Suit', description: ''},
+  {id: 4, title: 'King Killed by Alternating Color', description: ''},
+  {id: 5, title: 'Current Streak of Winning Games', description: ''},
+  {id: 6, title: 'Longest Streak of Winning Games', description: ''},
+  {id: 7, title: 'Average Kings Killed', description: ''},
+  {id: 8, title: 'Total Kings Killed', description: ''},
+  {id: 9, title: 'Maximum Cards Used in the Killing of a King', description: ''},
+  {id: 10, title: 'Fewest Reserve Spots Used in a Win', description: ''},
+  {id: 11, title: 'Renegade', description: 'Killed All 4 Kings'},
+  {id: 12, title: 'Revolutionary', description: 'Won 5 Games'},
+  {id: 13, title: 'Threvolution', description: 'Won 3 Games in a Row'},
+  {id: 14, title: 'Seven Up', description: 'Won 7 Games in a Row'},
+  {id: 15, title: 'Bastille My Heart', description: 'Won 14 games'},
+  {id: 16, title: 'Triple Ninja', description: 'Killed All 4 Kings with the Same Value'},
+  {id: 17, title: 'Suited Up', description: 'Killed All 4 Kings with Same Suit'},
+  {id: 18, title: 'Rainbow Rebel', description: 'Killed All 4 Kings with Alternating Colors'},
+  {id: 19, title: 'Clean Kill', description: 'Win Game with No Cards Left in Reserve Spots'},
+  {id: 20, title: 'Patience', description: 'Used 14 or More Cards in the Killing of a King'},
+  {id: 21, title: 'Show of Force', description: 'Killed All 4 Kings Only Using 4 Reserve Spots'},
+  {id: 22, title: 'Far, Far Better', description: 'No More Than 1 Reserve Spot was Ever Used at One Time'},
+  {id: 23, title: 'Rabble Rouser', description: 'Followed Standard Games on Twitter'},
+  {id: 24, title: 'Voice of the People', description: 'Tweeted A Victory'}
+]
+
+
+]
--- src/Routes/Achievements.js
+++ src/Routes/Achievements.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Grid, Menu, Header, Button, Container, Image } from 'semantic-ui-react'
+import { Grid, Menu, Header, Button, Container, Image, Card, Icon } from 'semantic-ui-react'
 
 class Achievements extends Component {
+
+  handleGameClick = () => {
+    this.props.history.push('/game')
+  }
+
+  handleMainMenuClick = () => {
+    this.props.history.push('/')
+  }
+
   render() {
     return (
-      <div className='main-menu-background'>
-      <Container style={ {width:'100vh',
+      <Grid stac className='main-menu-background'>
+      <Container stacked='true' style={ {width:'100vh',
     height:'100vh'} }>
       <Container style={ {width:'100%',
-    height:'50%'} }>
-      <Image src={ require('../Images/OtherImages/achievements.png') } alt='Achievements'/>
+    height:'60%'} } >
+      <Image centered src={ require('../Images/OtherImages/achievements.png') } alt='Achievements'/>
+      <br/>
+      <Button floated='right' size='mini' basic color='red'
+        onClick={this.handleMainMenuClick}>
+          Main Menu
+      </Button>
+      <Button size='mini' basic color='red'
+        onClick={this.handleGameClick}>
+          {'Play Game'}
+      </Button>
+<Container style={{backgroundColor: 'rgb(0, 66, 51)', width:'50%', marginTop:'10px', borderRadius:'10%'}}>
+    <Image centered size='small' src={require('../Images/OtherImages/killthekings_header.png')} />
+      <Header textAlign='center' style={{color: 'yellow'}}>THREVOLUTION</Header>
+      <Header textAlign='center' style={{color: 'white'}}>Won 3 Games in a Row</Header>
+      <Header textAlign='center' style={{color: 'white'}}>0</Header>
+  </Container>
       </Container>
       <Container style={ {width:'100%',
-    height:'50%', overflow: 'scroll'} }>
+    height:'40%', overflow: 'scroll'} }>
       <Menu fluid vertical color='pink'>
        <Menu.Item name='run'  />
        <Menu.Item name='walk' />
@@ -30,7 +54,7 @@ class Achievements extends Component {
      </Menu>
      </Container>
      </Container>
-     </div>
+     </Grid>
     )
   }
 }
