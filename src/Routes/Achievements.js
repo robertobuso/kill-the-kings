91d39a22e9ce5a3d1c431f574b56b0da9c1f3d83
robertobuso <robertobuso@gmail.com>
2019-01-20 16:22:57 -0500

    cleaned up achievements css a bit

--- src/Routes/Achievements.js
+++ src/Routes/Achievements.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Grid, Menu, Header, Button, Container, Image, Card, Icon } from 'semantic-ui-react'
+import { Grid, Menu, Header, Button, Container, Image } from 'semantic-ui-react'
 
 class Achievements extends Component {
 
@@ -14,11 +14,11 @@ class Achievements extends Component {
 
   render() {
     return (
-      <Grid stac className='main-menu-background'>
-      <Container stacked='true' style={ {width:'100vh',
+      <Grid stackable className='main-menu-background'>
+      <Container style={ {width:'100vh',
     height:'100vh'} }>
       <Container style={ {width:'100%',
-    height:'60%'} } >
+    height:'50%'} } >
       <Image centered src={ require('../Images/OtherImages/achievements.png') } alt='Achievements'/>
       <br/>
       <Button floated='right' size='mini' basic color='red'
@@ -29,15 +29,14 @@ class Achievements extends Component {
         onClick={this.handleGameClick}>
           {'Play Game'}
       </Button>
-<Container style={{backgroundColor: 'rgb(0, 66, 51)', width:'50%', marginTop:'10px', borderRadius:'10%'}}>
+<Container style={{backgroundColor: 'rgb(0, 66, 51)', width:'50%', marginTop:'20px', borderRadius:'10%'}}>
     <Image centered size='small' src={require('../Images/OtherImages/killthekings_header.png')} />
       <Header textAlign='center' style={{color: 'yellow'}}>THREVOLUTION</Header>
       <Header textAlign='center' style={{color: 'white'}}>Won 3 Games in a Row</Header>
       <Header textAlign='center' style={{color: 'white'}}>0</Header>
   </Container>
       </Container>
-      <Container style={ {width:'100%',
-    height:'40%', overflow: 'scroll'} }>
+      <Container style={ {width:'100%', height:'50%', overflow: 'scroll'} }>
       <Menu fluid vertical color='pink'>
        <Menu.Item name='run'  />
        <Menu.Item name='walk' />
